adf577431b48b4fce7913ae15a290ab768701935
MaximArzhanov <arzhanov93@yandex.ru>
2022-06-22 12:27:11 +0600

    refactor: Изменена логика хранения данных в localstorage

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -13,15 +13,15 @@ export const authAPI = {
     return new Promise(function (resolve, reject) {
 
       let users = [];
+      const result = {};
 
-      if (localStorage.getItem('users')) {
+      if (localStorage.getItem('users')) {  // Если в localStorage есть массив пользователей то массив сохраняется в переменную
         users = JSON.parse(localStorage.getItem('users'));
       } else {
-        localStorage.setItem('users', []);
+        localStorage.setItem('users', []);  // Если в localStorage нет массива пользователей то записывается пустой массив
       }
 
       const user = { email: email, password: password };
-      const result = {};
 
       if (users.some((user) => (user.email === email))) {
         result.isError = true;
@@ -36,35 +36,29 @@ export const authAPI = {
         result.message = USER_REGISTERED_SUCCESS_MESSAGE;
         resolve({ ...result });
       }
-
-      // if (JSON.parse(localStorage.getItem(email))) { // Проверяет существует ли уже пользователь с такой почтой
-      //   result.isError = true;
-      //   result.isAuthSuccess = false;
-      //   result.message = USER_ALREADY_EXIST_MESSAGE;
-      //   reject({ ...result });
-      // } else {
-      //   localStorage.setItem(email, JSON.stringify(user));
-      //   result.isError = false;
-      //   result.isAuthSuccess = true;
-      //   result.message = USER_REGISTERED_SUCCESS_MESSAGE;
-      //   resolve({ ...result });
-      // }
     });
   },
 
   /* Обеспечивает вход пользователя в систему */
   loginUser(email, password) {
     return new Promise(function (resolve, reject) {
-      const user = JSON.parse(localStorage.getItem(email));
+
+      let users = [];
       const result = {};
 
-      if (user) { // Проверяет наличие пользователя
+      if (localStorage.getItem('users')) {  // Если в localStorage есть массив пользователей то массив сохраняется в переменную
+        users = JSON.parse(localStorage.getItem('users'));
+      }
+
+      const user = users.find((user) => user.email === email);
+
+      if (user) {
         if (user.password === password) {
           result.isError = false;
           result.isAuthSuccess = true;
           result.message = USER_LOGINED_SUCCESS_MESSAGE;
           result.user = user;
-          resolve({ ...result});
+          resolve({ ...result });
         } else {
           result.isError = true;
           result.isAuthSuccess = false;
@@ -76,7 +70,6 @@ export const authAPI = {
         result.isAuthSuccess = false;
         result.message = USER_DOES_NOT_EXIST_MESSAGE;
         reject({ ...result });
-
       }
     });
   }
