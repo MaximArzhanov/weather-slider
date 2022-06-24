95ec1a528387f5444638df4211d64c32571ee182
MaximArzhanov <arzhanov93@yandex.ru>
2022-06-24 14:20:41 +0600

    refactor: в файле authReducer изменения в стейт осуществялются через деструктуризацию

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,63 +16,63 @@ const authReducer = (state = initialState, action) => {
   switch (action.type) {
 
     case CHANGE_TEXT_INPUT: {
-      const newState = { ...state };
-      newState.inputTexts = { ...state.inputTexts };
-      newState.inputTexts[action.inputID] = action.text; // Задаёт объекту inputTexts свойство с именем поля (Поле которое изменяется в данный момент)
-      return newState;
+      return {
+        ...state,
+        inputTexts: {
+          ...state.inputTexts,
+          [action.inputID]: action.text // Задаёт объекту inputTexts свойство с именем поля (Поле которое изменяется в данный момент)
+        }
+      }
     }
 
     case CLEAR_TEXT_INPUTS: {
-      const newState = { ...state };
-      newState.inputTexts = { ...state.inputTexts };
-      for (var key in newState.inputTexts) { newState.inputTexts[key] = '' }; // Перебор всех свойств в объекте newState.inputTexts (Сброс полей ввода)
-      return newState;
+      return { ...state, inputTexts: {} }
     }
 
     case CLEAR_ERROR_INPUTS: {
-      const newState = { ...state };
-      newState.inputValidationErrors = { ...state.inputValidationErrors };
-      for (let key in newState.inputValidationErrors) { newState.inputValidationErrors[key] = '' }; // Перебор всех свойств в объекте newState.inputValidationErrors (Сброс ошибок валидации полей input)
-      return newState;
+      return { ...state, inputValidationErrors: {} }
     }
 
     case CHANGE_ERROR_INPUT_TEXT: {
-      const newState = { ...state };
-      newState.inputValidationErrors = { ...state.inputValidationErrors };
-      newState.inputValidationErrors[action.inputID] = action.errorText; // Задаёт объекту inputTexts свойство с именем поля (Поле которое изменяется в данный момент)
-      return newState;
+      return {
+        ...state,
+        inputValidationErrors: {
+          ...state.inputValidationErrors,
+          [action.inputID]: action.errorText // Задаёт объекту inputValidationErrors свойство с именем поля (Поле которое изменяется в данный момент)
+        }
+      }
     }
 
     case CHANGE_FORM_VALIDITY_STATE: {
-      const newState = { ...state };
-      newState.isFormValid = action.state;
-      return newState;
+      return { ...state, isFormValid: action.state }
     }
 
     case LOGIN_USER: {
-      const newState = { ...state };
-      newState.authResult = { ...action.result };
-      return newState;
+      return { ...state, authResult: { ...action.result } }
     }
 
     case REGISTER_USER: {
-      const newState = { ...state };
-      newState.authResult = { ...action.result };
-      return newState;
+      return { ...state, authResult: { ...action.result } }
     }
 
     case RESET_AUTH_RESULT: {
-      const newState = { ...state };
-      newState.authResult = { ...state.authResult };
-      newState.authResult.isAuthSuccess = false;
-      return newState;
+      return {
+        ...state,
+        authResult: {
+          ...state.authResult,
+          isAuthSuccess: false
+        }
+      }
     }
 
     case CLEAR_AUTH_ERROR_TEXT: {
-      const newState = { ...state };
-      newState.authResult = { ...state.authResult };
-      newState.authResult.message = '';
-      return newState;
+      return {
+        ...state,
+        authResult: {
+          ...state.authResult,
+          message: ''
+        }
+      }
     }
 
     default:
