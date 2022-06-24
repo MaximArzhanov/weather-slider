070fc870cb10d7dc833433b919dd79aba7a96332
MaximArzhanov <arzhanov93@yandex.ru>
2022-06-24 15:40:59 +0600

    refactor: в файле weatherPageReducer.js  изменения в стейт осуществялются через деструктуризацию

--- src/store/reducers/weatherPageReducer.js
+++ src/store/reducers/weatherPageReducer.js
@@ -17,78 +17,80 @@ const weatherPageReducer = (state = initialState, action) => {
   switch (action.type) {
 
     case CHANGE_SEARCH_TEXT: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.inputTexts = { ...state.search.inputTexts };
-      newState.search.inputTexts[action.inputID] = action.text; // Задаёт объекту inputTexts свойство с именем поля (Поле которое изменяется в данный момент)
-      return newState;
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          inputTexts: { ...state.search.inputTexts, [action.inputID]: action.text }
+        }
+      }
     }
 
     case CHANGE_SEARCH_ERROR_INPUT_TEXT: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.inputValidationErrors = { ...state.search.inputValidationErrors };
-      newState.search.inputValidationErrors[action.inputID] = action.errorText; // Задаёт объекту inputTexts свойство с именем поля (Поле которое изменяется в данный момент)
-      return newState;
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          inputValidationErrors: { ...state.search.inputValidationErrors, [action.inputID]: action.errorText }
+        }
+      }
     }
 
     case CHANGE_SEARCH_FORM_VALIDITY_STATE: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.isFormValid = action.state;
-      return newState;
+      return {
+        ...state,
+        search: { ...state.search, isFormValid: action.state }
+      }
     }
 
     case CLEAR_SEARCH_TEXT_INPUTS: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.inputTexts = { ...state.search.inputTexts };
-      for (var key in newState.search.inputTexts) { newState.search.inputTexts[key] = '' }; // Перебор всех свойств в объекте newState.inputTexts (Сброс полей ввода)
-      return newState;
+      return {
+        ...state,
+        search: { ...state.search, inputTexts: {} }
+      }
     }
 
     case CLEAR_SEARCH_ERROR_INPUTS: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.inputValidationErrors = { ...state.search.inputValidationErrors };
-      for (let key in newState.search.inputValidationErrors) { newState.search.inputValidationErrors[key] = '' }; // Перебор всех свойств в объекте newState.inputValidationErrors (Сброс ошибок валидации полей input)
-      return newState;
+      return {
+        ...state,
+        search: { ...state.search, inputValidationErrors: {} }
+      }
     }
 
     case UPDATE_SEARCH_WEATHER_ERROR: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.searchResult = { ...action.result };
-      return newState;
+      return {
+        ...state,
+        search: { ...state.search,  searchResult: { ...action.result } }
+      }
     }
 
     case ADD_NEW_CARD_CITY_WEATHER: {
-      const newState = { ...state };
-      newState.weatherCards = [ ...state.weatherCards ];
-      newState.weatherCards.push(action.currentCityWeather);
-      return newState;
+      return {
+        ...state,
+        weatherCards: [ ...state.weatherCards, { ...action.currentCityWeather } ]
+      }
     }
 
     case RESET_SEARCH_ERROR_TEXT: {
-      const newState = { ...state };
-      newState.search = { ...state.search };
-      newState.search.searchResult = { ...state.search.searchResult };
-      newState.search.searchResult.isError = false;
-      newState.search.searchResult.isRequestSuccessful = true;
-      newState.search.searchResult.message = '';
-      return newState;
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          searchResult: {
+            isError: false,
+            isRequestSuccessful: true,
+            message: ''
+          }
+        }
+      }
     }
 
     case INITIAL_CITY_WEATHER_CARDS: {
-      const newState = { ...state };
-      newState.weatherCards = [ ...action.weatherCards ];
-      return newState;
+      return { ...state, weatherCards: [ ...action.weatherCards ] }
     }
 
     case RESET_WEATHER_CARDS: {
-      const newState = { ...state };
-      newState.weatherCards = [];
-      return newState;
+      return { ...state, weatherCards: [] }
     }
 
     default:
