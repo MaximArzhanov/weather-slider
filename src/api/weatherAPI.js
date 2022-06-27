48b026f9379e0c56d31fd3ca3c5b38484a379a92
MaximArzhanov <arzhanov93@yandex.ru>
2022-06-27 18:07:50 +0600

    feat: Создан метод для запроса истории погоды от WeatherAPI

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -3,23 +3,36 @@ import * as axios from "axios";
 const API_KEY = '11a4ada72f2e45f2bf065312222106';
 
 const instance = axios.create({
-    withCredentials: false,
-    baseURL: 'http://api.weatherapi.com/v1',
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    }
+  withCredentials: false,
+  baseURL: 'http://api.weatherapi.com/v1',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  }
 });
 
 export const weatherAPI = {
 
-    getCurrentWeather(cityName) {
-        return instance.get('/current.json', {
-          params: {
-            key: API_KEY,
-            q: cityName,
-            lang: 'ru'
-          }
-        })
-          .then((response) => { return response.data });
-    },
+  getCurrentWeather(cityName) {
+    return instance.get('/current.json', {
+      params: {
+        key: API_KEY,
+        q: cityName,
+        lang: 'ru'
+      }
+    })
+      .then((response) => { return response.data });
+  },
+
+  getHistoryWeather(cityName, data) {
+    return instance.get('/history.json', {
+      params: {
+        key: API_KEY,
+        q: cityName,
+        lang: 'ru',
+        dt: data
+      }
+    })
+      .then((response) => { return response.data });
+  },
+
 }
